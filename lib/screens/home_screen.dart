7b5c5ee773b444115610419d1bfefe376a3a2542
Usaid Asif <usaidasif12@gmail.com>
2023-06-02 21:04:23 +0500

    Created bottom appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +9,80 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+              icon: Icon(
+                Icons.home,
+                size: 30,
+                color: currentPage == 0 ? Colors.black87 : Colors.black54,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 1;
+                });
+              },
+              icon: Icon(
+                Icons.search,
+                size: 30,
+                color: currentPage == 1 ? Colors.black87 : Colors.black54,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 2;
+                });
+              },
+              icon: Icon(
+                Icons.add,
+                size: 30,
+                color: currentPage == 2 ? Colors.black87 : Colors.black54,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 3;
+                });
+              },
+              icon: Icon(
+                Icons.ondemand_video,
+                size: 30,
+                color: currentPage == 3 ? Colors.black87 : Colors.black54,
+              ),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentPage = 4;
+                });
+              },
+              icon: Icon(
+                FontAwesomeIcons.user,
+                size: 30,
+                color: currentPage == 4 ? Colors.black87 : Colors.black54,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
