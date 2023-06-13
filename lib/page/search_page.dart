ed2009c8c5c4d2892ccd415c95165963225ae5cf
Usaid Asif <usaidasif12@gmail.com>
2023-06-13 19:42:30 +0500

    created search page

--- lib/page/search_page.dart
+++ lib/page/search_page.dart
@@ -5,6 +5,39 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: TextFormField(
+          decoration: InputDecoration(
+            prefixIcon: const Icon(
+              Icons.search,
+            ),
+            hintText: 'Search',
+            contentPadding: const EdgeInsets.all(0),
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            fillColor: const Color.fromRGBO(
+              220,
+              220,
+              220,
+              1,
+            ),
+            filled: true,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.person_add,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
